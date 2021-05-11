11c314ea953c20ee7b2e33efa93352ca98c1e243
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-11 11:23:01 +0900

    :up:マイグレーションファイルを1つに統一
    以下ご指摘の対応
    https://github.com/nisimotoryoga/pbl_fp_matching/pull/31/files#r629785445
    
    ロールバックして統合

--- db/migrate/20210506070252_add_column_to_clients.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToClients < ActiveRecord::Migration[6.1]
-  def change
-    add_column :clients, :name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_070252) do
+ActiveRecord::Schema.define(version: 2021_05_06_070235) do
 
   create_table "clients", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2021_05_06_070252) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.index ["email"], name: "index_clients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
   end
