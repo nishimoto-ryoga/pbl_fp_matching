1fc3895c467e233192efba859d332236b4c081c1
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-11 11:40:33 +0900

    :up:ユーザー登録時に名前を登録できるようにする
    viewの修正とcontrollerにストロングパラメータを追記
    別ブランチで対応したがまだマージできていないので、こちらでもマイグレーションファイルを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # ユーザー登録時に名前を登録できるようにする
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
