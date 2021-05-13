092342fac2423ca8df455f1e8049a40af63863ad
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-13 11:52:19 +0900

    :up:参照ページをコメントアウトで追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   # ユーザー登録時に名前を登録できるようにする
+  # https://github.com/heartcombo/devise#strong-parameters
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
