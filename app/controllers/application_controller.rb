ac871cfcbfa56c8a63b36284621366c6d920cd86
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-14 12:55:25 +0900

    :up:コメント削除
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/36#discussion_r632245151

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # ログイン後遷移先指定
   def after_sign_in_path_for(resource)
-    # FPの場合はFPのマイページに飛ばす
     planner_path(current_planner.id) if planner_signed_in?
-    # クライアントの場合はクライアントのマイページに飛ばす
     client_path(current_client.id) if client_signed_in?
   end
   
-  # ログアウト後後遷移先指定
   def after_sign_out_path_for(resource)
     if resource == :planner
-        top_fp_path
+      top_fp_path
     else
-        root_path
+      root_path
     end
   end
 
