9e0a022add9fc67790152529c853392003111b9c
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-14 17:30:07 +0900

    :new:Plannerのアクセスに関するRSpec対応
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/36#discussion_r632243922
    対応したこと
    ・factory_bot_rails gemのインストール
    ・マイページのアクセスについてログイン前後の挙動についてテストを書く

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development, :test do
 
   # 追加
   gem 'rspec-rails', '~> 5.0.0'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/requests/planners_spec.rb
+++ spec/requests/planners_spec.rb
@@ -1,18 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Planners", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/planners/show"
-      expect(response).to have_http_status(:success)
+RSpec.describe Planner, type: :request do
+  describe "マイページのアクセス" do
+    before do
+      @planner = FactoryBot.create(:planner)
+    end
+    context "ログイン前" do
+      it "sign_inにリダイレクトされる" do
+        get planner_path(@planner.id)
+        expect(response).to redirect_to new_planner_session_path
+      end
+    end
+    context "ログイン後" do
+      before do
+        sign_in @planner
+      end
+      it "正常なレスポンスが返ってくる(200)" do
+        get planner_path(@planner.id)
+        expect(response.status).to eq 200
+      end
+      it "plannerのnameが表示される" do
+        get planner_path(@planner.id)
+        expect(response.body).to include @planner.name
+      end
     end
   end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/planners/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
