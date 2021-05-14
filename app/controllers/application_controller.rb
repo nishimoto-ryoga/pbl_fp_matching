95783cf4307b0b10374714fb204cdb303fc5e940
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-14 10:29:20 +0900

    :up:ログイン後・ログアウト後の遷移を指定
    ログイン後
    FPの場合はFPのマイページへ、clientの場合はclientのマイページへ
    ※clientのマイページは未実装
    ログアウト後
    FPの場合はFPのTOPページへ、clientの場合はrootへ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    # FPの場合はFPのマイページに飛ばす
+    planner_path(current_planner.id) if planner_signed_in?
+    # クライアントの場合はクライアントのマイページに飛ばす
+    client_path(current_client.id) if client_signed_in?
+  end
+
+  def after_sign_out_path_for(resource)
+      if resource == :planner
+          top_fp_path
+      else
+          root_path
+      end
+  end
+
   protected
 
   # ユーザー登録時に名前を登録できるようにする
