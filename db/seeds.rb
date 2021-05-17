d34af700e6cee5fac459aec1fdc64ddb6750af5d
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-17 22:21:32 +0900

    :up:インデント不正の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 # 10時〜18:00まで30分単位で枠を作成(計16枠)
 16.times do |n|
   TimeFrame.create!(
-      start_at: (Time.zone.parse("10:00") + 30.minutes * n).strftime("%H:%M"),
-      end_at: (Time.zone.parse("10:30") + 30.minutes * n).strftime("%H:%M")
+    start_at: (Time.zone.parse("10:00") + 30.minutes * n).strftime("%H:%M"),
+    end_at: (Time.zone.parse("10:30") + 30.minutes * n).strftime("%H:%M")
   )
 end
