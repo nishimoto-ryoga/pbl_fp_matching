386494784bd44eba8e0296bbbc27a5a51042c228
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-18 00:22:05 +0900

    :up:modelにhas_manyを記述
    リレーションの記述が抜けていたので追記
    親レコードの削除時の処理についてはコメントアウト参照

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -3,4 +3,7 @@ class Planner < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # 予約枠を辿るときにPlannerがわからないという状況を防ぐ
+  has_many :reservation_frames, :restrict_with_exception
 end
--- app/models/time_frame.rb
+++ app/models/time_frame.rb
@@ -1,2 +1,4 @@
 class TimeFrame < ApplicationRecord
+  # 予約枠を辿るときにTimeFrameがわからないという状況を防ぐ
+  has_many :reservation_frames, :restrict_with_exception
 end
