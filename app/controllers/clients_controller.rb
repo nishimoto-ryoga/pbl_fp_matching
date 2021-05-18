8a9590eaa58f05b3f918f834019b93d973715678
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-18 16:31:03 +0900

    :up:clients/showはログインユーザーのみ閲覧できるように制御
    現在はshowアクションのみだが、ゆくゆくeditでも必要になることを考えてbefore_actionでまとめた
    pramasの取得を安全に行うためにprivate以下に記述

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,11 +1,20 @@
 class ClientsController < ApplicationController
   # Clientでログインしていなければsign_inページにリダイレクトされる
   before_action :authenticate_client!
+  before_action :client_check, only: [:show, :edit]
   
   def show
     @client = Client.find(params[:id])
   end
-
+  
   def edit
   end
+  
+  private
+  
+  def client_check
+    @client = Client.find(params[:id])
+    redirect_to client_path(current_client.id) unless @client.id == current_client.id
+  end
+  
 end
