50eab03f34d02f758cfe5e4b914c9bc5ec8ec6cc
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-18 22:18:24 +0900

    :up:ReservationFrameのcreateに関するRSpec追加
    date,planner_id,time_frame_idで作成されることと、
    そのいずれかが欠けたら作成できないことをテスト
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/42#discussion_r634231759

--- spec/requests/reservation_frames_spec.rb
+++ spec/requests/reservation_frames_spec.rb
@@ -1,11 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe "ReservationFrames", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/reservation_frames/new"
-      expect(response).to have_http_status(:success)
-    end
+RSpec.describe "ReservationFrames", type: :model do
+  let!(:planner) { FactoryBot.create(:planner) }
+  let!(:time_frame) { FactoryBot.create(:time_frame) }
+  it "正常に作成される" do
+    reservation_frame = ReservationFrame.new(
+      date: Time.zone.now.to_date,
+      time_frame_id: time_frame.id,
+      planner_id: planner.id
+    )
+    expect(reservation_frame).to be_valid
   end
-
+  it "time_frame_idが無く作成されず" do
+    reservation_frame = ReservationFrame.new(
+      date: Time.zone.now.to_date,
+      time_frame_id: time_frame.id,
+      planner_id: nil
+    )
+    expect(reservation_frame.valid?).to eq(false)
+  end
+  it "planner_idが無く作成されず" do
+    reservation_frame = ReservationFrame.new(
+      date: Time.zone.now.to_date,
+      time_frame_id: time_frame.id,
+      planner_id: nil
+    )
+    expect(reservation_frame.valid?).to eq(false)
+  end
+  it "dateが無く作成されず" do
+    reservation_frame = ReservationFrame.new(
+      date: nil,
+      time_frame_id: time_frame.id,
+      planner_id: planner.id
+    )
+    expect(reservation_frame.valid?).to eq(false)
+  end
 end
