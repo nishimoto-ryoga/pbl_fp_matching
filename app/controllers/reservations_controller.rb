3042065dc295260dd298ac1df18ae6e7feb20064
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-21 17:27:08 +0900

    :new:createアクション追加
    paramsでreservation_frameのidを受け取り、
    現在ログインしているclientのidを紐付けて保存する

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,4 +4,19 @@ class ReservationsController < ApplicationController
     @reservation_frame = ReservationFrame.find(params[:reservation_frame_id])
   end
 
+  def create
+    reservation = Reservation.new(reservation_params)
+    reservation.client_id = current_client.id
+    if reservation.save
+      redirect_to mypage_clients_path
+    else
+      redirect_to planners_path
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reservation_frame_id)
+  end
 end
