b7a91c9b719562ede52378efd028fffd5d767328
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-24 13:49:14 +0900

    :new:Reservationに関するRSpec作成
    client_idとreservation_frame_idがあれば有効であることと、いずれかが欠けていれば無効であることをテスト

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -1,10 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Reservations", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/reservations/new"
-      expect(response).to have_http_status(:success)
+RSpec.describe "Reservations", type: :model do
+  let!(:client) { FactoryBot.create(:client) }
+  let!(:reservation_frame) { FactoryBot.create(:reservation_frame, date: Time.zone.now.to_date) }
+
+  context 'client_id, reservation_frame_id がすべて設定されている場合' do
+    it "reservationが有効" do
+      reservation = Reservation.new(
+        reservation_frame_id: reservation_frame.id,
+        client_id: client.id
+      )
+      expect(reservation).to be_valid
+    end
+  end
+  context 'client_id が設定されていない場合' do
+    it "reservationが無効" do
+      reservation = Reservation.new(
+        reservation_frame_id: reservation_frame.id,
+        client_id: nil
+      )
+      expect(reservation.valid?).to eq(false)
+    end
+  end
+  context 'reservation_frame_id が設定されていない場合' do
+    it "reservationが無効" do
+      reservation = Reservation.new(
+        reservation_frame_id: nil,
+        client_id: client.id
+      )
+      expect(reservation.valid?).to eq(false)
     end
   end
 
