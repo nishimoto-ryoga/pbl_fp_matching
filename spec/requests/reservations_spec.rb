db795ea3b278655937478753d518e9bfd4d42b7d
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-25 14:26:35 +0900

    :up:itをワンライナーでの記述に修正
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/57#discussion_r638421113

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -8,23 +8,17 @@ RSpec.describe Reservation, type: :model do
   context 'client_id, reservation_frame_id がすべて設定されている場合' do
     let!(:reservation_frame_id) { reservation_frame.id }
     let!(:client_id) { client.id }
-    it "reservationが有効" do
-      is_expected.to be_valid
-    end
+    it { is_expected.not_to be_valid }
   end
   context 'client_id が設定されていない場合' do
     let!(:reservation_frame_id) { reservation_frame.id }
     let!(:client_id) { nil }
-    it "reservationが無効" do
-      is_expected.not_to be_valid
-    end
+    it { is_expected.not_to be_valid }
   end
   context 'reservation_frame_id が設定されていない場合' do
     let!(:reservation_frame_id) { nil }
     let!(:client_id) { client.id }
-    it "reservationが無効" do
-      is_expected.not_to be_valid
-    end
+    it { is_expected.not_to be_valid }
   end
   
 end
