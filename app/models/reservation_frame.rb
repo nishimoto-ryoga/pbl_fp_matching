c883ed1a7153c362e008ba04abf0813f635a4925
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-27 13:54:05 +0900

    :up:modelの書き方修正
    ・bool値を返すメソッドには ? をつける
    ・関連付けを利用する
    ・has_oneのときは単数形
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/60#discussion_r640254597

--- app/models/reservation_frame.rb
+++ app/models/reservation_frame.rb
@@ -1,13 +1,13 @@
 class ReservationFrame < ApplicationRecord
   belongs_to :planner
   belongs_to :time_frame
-  has_one :reservations, dependent: :restrict_with_exception
+  has_one :reservation, dependent: :restrict_with_exception
 
   validates :date, presence: true
 
   scope :not_canceled, -> { where(canceled_at: nil) }
 
-  def is_reserved
-    return Reservation.exists?(reservation_frame_id: id)
+  def is_reserved?
+    return reservation.present?
   end
 end
