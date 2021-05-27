b039f4ac863cccb574e3be345a7240b0a1c7d83e
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-05-27 14:03:36 +0900

    :up:canceled_atのdefaltをnullに変更
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/60#discussion_r640254953

--- db/migrate/20210526124149_rename_is_deleted_column_to_reservation_frames.rb
+++ db/migrate/20210526124149_rename_is_deleted_column_to_reservation_frames.rb
@@ -1,6 +1,11 @@
 class RenameIsDeletedColumnToReservationFrames < ActiveRecord::Migration[6.1]
   def up
     rename_column :reservation_frames, :is_deleted, :canceled_at
-    change_column :reservation_frames, :canceled_at, :datetime, :default => '', :null =>  true
+    change_column :reservation_frames, :canceled_at, :datetime, :default => nil, :null =>  true
   end
+
+  def down
+    rename_column :reservation_frames, :canceled_at, :is_deleted
+  end
+  
 end
