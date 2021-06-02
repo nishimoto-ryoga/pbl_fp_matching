aa0ed87ab386d52f807bfeab478c5d94e924199e
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-02 23:59:00 +0900

    :up:予約枠作成時に要件に沿ってバリデーションを作成
    以下は予約枠の作成ができない
    ・過去と1年以上先
    ・日曜日
    ・土曜日の11時〜15時以外

--- app/models/reservation_frame.rb
+++ app/models/reservation_frame.rb
@@ -3,9 +3,10 @@ class ReservationFrame < ApplicationRecord
   belongs_to :time_frame
   has_one :reservation, dependent: :restrict_with_exception
 
-  validates :date, presence: true
-
+  validates :date, presence: true, uniqueness: { scope: %i[time_frame_id planner] }
   scope :not_canceled, -> { where(canceled_at: nil) }
+
+  validate :sunday_is_closed, :saturday_is_shorttime, :invalid_of_date, on: :create
 
   def reserved?
     reservation.present?
@@ -13,5 +14,23 @@ class ReservationFrame < ApplicationRecord
 
   def canceled?
     canceled_at?
+  end
+
+  def sunday_is_closed
+    return unless date.wday.zero?
+
+    errors.add(:date, '：日曜日はゆっくり過ごしましょう。')
+  end
+
+  def saturday_is_shorttime
+    return unless date.wday == 6 && !time_frame_id.between?(3, 11)
+
+    errors.add(:date, '：土曜日は11時〜15時の間で指定してください。')
+  end
+
+  def invalid_of_date
+    return unless date <= Time.zone.today || date > Time.zone.today.since(1.year)
+
+    errors.add(:date, '：明日以降かつ1年以内で設定してください。')
   end
 end
