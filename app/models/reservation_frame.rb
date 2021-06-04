a2f8460e5361ece126af866a602555568a9dd51f
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-04 17:01:56 +0900

    :up:reservation_frameモデルの指摘ポイント修正
    指摘コメント：
    https://github.com/nisimotoryoga/pbl_fp_matching/pull/72#discussion_r644636163
    https://github.com/nisimotoryoga/pbl_fp_matching/pull/72#discussion_r644639444
    https://github.com/nisimotoryoga/pbl_fp_matching/pull/72#discussion_r645250738

--- app/models/reservation_frame.rb
+++ app/models/reservation_frame.rb
@@ -6,7 +6,9 @@ class ReservationFrame < ApplicationRecord
   validates :date, presence: true, uniqueness: { scope: %i[time_frame_id planner] }
   scope :not_canceled, -> { where(canceled_at: nil) }
 
-  validate :sunday_is_closed, :saturday_is_shorttime, :invalid_of_date, on: :create
+  validate :the_reservation_on_sunday_is_denied
+  validate :saturday_is_shorttime
+  validate :the_date_in_the_past_or_over_a_year_after_today_is_invalid
 
   def reserved?
     reservation.present?
@@ -16,21 +18,21 @@ class ReservationFrame < ApplicationRecord
     canceled_at?
   end
 
-  def sunday_is_closed
-    return unless date.wday.zero?
+  def the_reservation_on_sunday_is_denied
+    return unless date.sunday?
 
-    errors.add(:date, '：日曜日はゆっくり過ごしましょう。')
+    errors.add(:date, '日曜日はゆっくり過ごしましょう。')
   end
 
   def saturday_is_shorttime
     return unless date.wday == 6 && !time_frame_id.between?(3, 11)
 
-    errors.add(:date, '：土曜日は11時〜15時の間で指定してください。')
+    errors.add(:date, '土曜日は11時〜15時の間で指定してください。')
   end
 
-  def invalid_of_date
+  def the_date_in_the_past_or_over_a_year_after_today_is_invalid
     return unless date <= Time.zone.today || date > Time.zone.today.since(1.year)
 
-    errors.add(:date, '：明日以降かつ1年以内で設定してください。')
+    errors.add(:date, '明日以降かつ1年以内で設定してください。')
   end
 end
