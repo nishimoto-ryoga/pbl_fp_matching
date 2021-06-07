fdb77a3de3f3e2d99d36ac0b3c541f189bdb2395
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-07 15:35:50 +0900

    :up:4バイト以上の文字をニックネームに入力できないようにする
    4バイト以上の文字(=絵文字)をカスタムバリデーションではじく

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,5 +5,5 @@ class Client < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reservations, dependent: :restrict_with_exception
-  validates :name, presence: true, length: { maximum: 10, message: '10文字以内です' }
+  validates :name, presence: true, length: { maximum: 10, message: '10文字以内です' }, chars4bytes: true
 end
---
+++ app/validators/chars_4bytes_validator.rb
@@ -0,0 +1,10 @@
+class Chars4bytesValidator < ActiveModel::EachValidator
+  # UTF-8で4バイト以上の文字かを判定する
+  def validate_each(record, attribute, value)
+    return if value.nil?
+
+    unavailable_chars = value.scan(/[^\u0000-\uFFFF]/)
+
+    record.errors.add(attribute, message: '使用できない文字が含まれています') if unavailable_chars.present?
+  end
+end
