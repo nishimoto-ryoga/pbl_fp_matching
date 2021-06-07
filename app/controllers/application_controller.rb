4e30e6957d0d23253b7e940751c6c439aa0ae6c9
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-07 17:13:49 +0900

    :up:ログイン中にもう一方のユーザーにログインしようとすると500エラーが出るのを解消する
    例えばplannersでログインしているときにclientページを閲覧しようとするなど
    ログインしていればmypageへリダイレクトさせることで回避

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :client_cannot_login_to_planner
+  before_action :planner_cannot_login_to_client
 
   def after_sign_in_path_for(_resource)
     return mypage_planners_path if planner_signed_in?
@@ -9,6 +11,20 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     resource == :planner ? top_fp_path : root_path
+  end
+
+  def planner_cannot_login_to_client
+    return unless request.path.in?(['/clients/sign_up', '/clients/sign_in']) && planner_signed_in?
+
+    flash[:danger] = 'ログアウトしてから再度お試しください。'
+    redirect_to mypage_planners_path
+  end
+
+  def client_cannot_login_to_planner
+    return unless request.path.in?(['/planners/sign_up', '/planners/sign_in']) && client_signed_in?
+
+    flash[:danger] = 'ログアウトしてから再度お試しください。'
+    redirect_to mypage_clients_path
   end
 
   protected
