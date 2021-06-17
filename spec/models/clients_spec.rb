f4a28a667763384b59615468ddd02d279fb390a5
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-17 14:26:45 +0900

    :up:clientとplannerの作成できるかのテストを追加
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/79#discussion_r653166526

--- spec/models/clients_spec.rb
+++ spec/models/clients_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Client, type: :model do
   describe 'client作成' do
+    context '名前に絵文字を含まず、10文字未満の場合' do
+      it 'clientが作成される' do
+        client = Client.new(
+          email: 'a@a',
+          password: 'aaaaaa',
+          name: 'ニックネーム'
+        )
+        expect(client).to be_valid
+      end
+    end
     context '名前が10文字以上の場合' do
       it 'clientが作成されない' do
         client = Client.new(
--- spec/models/planner_spec.rb
+++ spec/models/planner_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Planner, type: :model do
   describe 'planner作成' do
+    context '名前に絵文字を含まず、10文字未満の場合' do
+      it 'plannerが作成される' do
+        planner = Planner.new(
+          email: 'a@a',
+          password: 'aaaaaa',
+          name: 'ニックネーム'
+        )
+        expect(planner).to be_valid
+      end
+    end
     context '名前が10文字以上の場合' do
       it 'plannerが作成されない' do
         planner = Planner.new(
