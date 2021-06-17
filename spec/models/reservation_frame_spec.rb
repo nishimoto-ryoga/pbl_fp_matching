3d4f4111411c49aeb30dade3dbd603b41d847a18
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-17 14:16:48 +0900

    :up:reservation_frameをDRYに書くためにletで定義し直す
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/79#discussion_r653167327

--- spec/models/reservation_frame_spec.rb
+++ spec/models/reservation_frame_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe ReservationFrame, type: :model do
   let!(:planner) { FactoryBot.create(:planner) }
   let!(:time_frame) { FactoryBot.create(:time_frame) }
+  let!(:reservation_frame) { ReservationFrame.new(time_frame_id: time_frame.id, planner_id: planner.id) }
 
   context 'date が設定されていない場合' do
     it 'reservation_frameが無効' do
       reservation_frame = ReservationFrame.new(
-        date: nil,
-        time_frame_id: time_frame.id,
-        planner_id: planner.id
+        date: nil
       )
       expect(reservation_frame.valid?).to eq(false)
     end
@@ -18,31 +17,20 @@ RSpec.describe ReservationFrame, type: :model do
     let!(:next_mon) { Time.zone.now.to_date + 8 - Time.zone.now.to_date.wday }
     context 'date, time_frame_id, planner_id がすべて設定されている場合' do
       it 'reservation_frameが有効' do
-        reservation_frame = ReservationFrame.new(
-          date: next_mon,
-          time_frame_id: time_frame.id,
-          planner_id: planner.id
-        )
+        reservation_frame.date = next_mon
         expect(reservation_frame).to be_valid
       end
     end
     context 'time_frame_id が設定されていない場合' do
       it 'reservation_frameが無効' do
-        reservation_frame = ReservationFrame.new(
-          date: next_mon,
-          time_frame_id: nil,
-          planner_id: planner.id
-        )
+        reservation_frame.date = next_mon
+        reservation_frame.time_frame_id = nil
         expect(reservation_frame.valid?).to eq(false)
       end
     end
     context 'planner_id が設定されていない場合' do
       it 'reservation_frameが無効' do
-        reservation_frame = ReservationFrame.new(
-          date: next_mon,
-          time_frame_id: time_frame.id,
-          planner_id: nil
-        )
+        reservation_frame.planner_id = nil
         expect(reservation_frame.valid?).to eq(false)
       end
     end
@@ -51,21 +39,15 @@ RSpec.describe ReservationFrame, type: :model do
     let!(:next_sta) { Time.zone.now.to_date + 13 - Time.zone.now.to_date.wday }
     context '10:00の場合' do
       it 'reservation_frameが無効' do
-        reservation_frame = ReservationFrame.new(
-          date: next_sta,
-          time_frame_id: time_frame.id,
-          planner_id: planner.id
-        )
+        reservation_frame.date = next_sta
         expect(reservation_frame.valid?).to eq(false)
       end
     end
     context '11:00の場合' do
+      let!(:other_time_frame) { TimeFrame.create(id: 3, start_at: '11:00', end_at: '11:30') }
       it 'reservation_frameが有効' do
-        reservation_frame = ReservationFrame.new(
-          date: next_sta,
-          time_frame_id: time_frame.id,
-          planner_id: planner.id
-        )
+        reservation_frame.date = next_sta
+        reservation_frame.time_frame_id = other_time_frame.id
         expect(reservation_frame).to be_valid
       end
     end
@@ -74,9 +56,7 @@ RSpec.describe ReservationFrame, type: :model do
     let!(:next_sun) { Time.zone.now.to_date + 7 - Time.zone.now.to_date.wday }
     it 'reservation_frameが無効' do
       reservation_frame = ReservationFrame.new(
-        date: next_sun,
-        time_frame_id: time_frame.id,
-        planner_id: planner.id
+        date: next_sun
       )
       expect(reservation_frame.valid?).to eq(false)
     end
@@ -84,9 +64,7 @@ RSpec.describe ReservationFrame, type: :model do
   context 'dateが過去日付の場合' do
     it 'reservation_frameが無効' do
       reservation_frame = ReservationFrame.new(
-        date: Time.zone.now.to_date - 1,
-        time_frame_id: time_frame.id,
-        planner_id: planner.id
+        date: Time.zone.now.to_date - 1
       )
       expect(reservation_frame.valid?).to eq(false)
     end
@@ -94,9 +72,7 @@ RSpec.describe ReservationFrame, type: :model do
   context 'dateが1年以上後の場合' do
     it 'reservation_frameが無効' do
       reservation_frame = ReservationFrame.new(
-        date: Time.zone.now.to_date + 400,
-        time_frame_id: time_frame.id,
-        planner_id: planner.id
+        date: Time.zone.now.to_date + 400
       )
       expect(reservation_frame.valid?).to eq(false)
     end
