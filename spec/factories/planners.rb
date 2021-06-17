0c28d19192940c82135c3a0caf632ad0aab828c4
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-17 12:32:39 +0900

    :up:factory_botでspaceで揃えていたのをやめる
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/79#discussion_r653137489

--- spec/factories/planners.rb
+++ spec/factories/planners.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :planner do
-    email                           { 'a@a' }
-    password                        { 'abc1234' }
-    name                            { 'ニックネーム' }
+    email { 'a@a' }
+    password { 'abc1234' }
+    name { 'ニックネーム' }
   end
 end
