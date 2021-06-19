538d95117cdbe4ba1c01e5f37c5729b68116907e
nisimotoryoga <nishimoto.ryoga@moneyforward.co.jp>
2021-06-20 00:21:10 +0900

    :up: let と subject を使って DRY にリファクタ
    ・初めに date を定義する必要があり、空で定義する
    ・以降、date を let で上書きする形で記述
    ・is_expected でよりDRY に書くため subject でreservation_frame を定義する
    指摘コメント：https://github.com/nisimotoryoga/pbl_fp_matching/pull/79#discussion_r654231395

--- spec/models/reservation_frame_spec.rb
+++ spec/models/reservation_frame_spec.rb
@@ -3,27 +3,19 @@ require 'rails_helper'
 RSpec.describe ReservationFrame, type: :model do
   let!(:planner) { FactoryBot.create(:planner) }
   let!(:time_frame) { FactoryBot.create(:time_frame) }
-  let!(:reservation_frame) { ReservationFrame.new(time_frame_id: time_frame.id, planner_id: planner.id) }
+  let!(:date) { nil }
+  subject!(:reservation_frame) { ReservationFrame.new(date: date, time_frame_id: time_frame.id, planner_id: planner.id) }
 
   context 'date が設定されていない場合' do
-    it 'reservation_frameが無効' do
-      reservation_frame = ReservationFrame.new(
-        date: nil
-      )
-      expect(reservation_frame.valid?).to eq(false)
-    end
+    it { is_expected.not_to be_valid }
   end
   context 'dateが月〜金のとき' do
-    let!(:weekday) { Date.current.next_week(%i[monday tuesday wednesday thursday friday].sample) }
+    let!(:date) { Date.current.next_week(%i[monday tuesday wednesday thursday friday].sample) }
     context 'date, time_frame_id, planner_id がすべて設定されている場合' do
-      it 'reservation_frameが有効' do
-        reservation_frame.date = weekday
-        expect(reservation_frame).to be_valid
-      end
+      it { is_expected.to be_valid }
     end
     context 'time_frame_id が設定されていない場合' do
       it 'reservation_frameが無効' do
-        reservation_frame.date = weekday
         reservation_frame.time_frame_id = nil
         expect(reservation_frame.valid?).to eq(false)
       end
@@ -36,45 +28,28 @@ RSpec.describe ReservationFrame, type: :model do
     end
   end
   context 'dateが土曜のとき' do
-    let!(:next_sta) { Time.zone.now.to_date + 13 - Time.zone.now.to_date.wday }
+    let!(:date) { Date.current.next_week(:saturday) }
     context '10:00の場合' do
-      it 'reservation_frameが無効' do
-        reservation_frame.date = next_sta
-        expect(reservation_frame.valid?).to eq(false)
-      end
+      it { is_expected.not_to be_valid }
     end
     context '11:00の場合' do
       let!(:other_time_frame) { TimeFrame.create(id: 3, start_at: '11:00', end_at: '11:30') }
       it 'reservation_frameが有効' do
-        reservation_frame.date = next_sta
         reservation_frame.time_frame_id = other_time_frame.id
         expect(reservation_frame).to be_valid
       end
     end
   end
   context 'dateが日曜日のとき' do
-    let!(:next_sun) { Time.zone.now.to_date + 7 - Time.zone.now.to_date.wday }
-    it 'reservation_frameが無効' do
-      reservation_frame = ReservationFrame.new(
-        date: next_sun
-      )
-      expect(reservation_frame.valid?).to eq(false)
-    end
+    let!(:date) { Date.current.next_week(:sunday) }
+    it { is_expected.not_to be_valid }
   end
-  context 'dateが過去日付の場合' do
-    it 'reservation_frameが無効' do
-      reservation_frame = ReservationFrame.new(
-        date: Time.zone.now.to_date - 1
-      )
-      expect(reservation_frame.valid?).to eq(false)
-    end
+  context 'dateが昨日(過去日付)の場合' do
+    let!(:date) { Time.zone.now.to_date - 1 }
+    it { is_expected.not_to be_valid }
   end
   context 'dateが1年以上後の場合' do
-    it 'reservation_frameが無効' do
-      reservation_frame = ReservationFrame.new(
-        date: Time.zone.now.to_date + 400
-      )
-      expect(reservation_frame.valid?).to eq(false)
-    end
+    let!(:date) { Time.zone.now.to_date + 400 }
+    it { is_expected.not_to be_valid }
   end
 end
